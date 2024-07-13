1cbe91a9e7f440ec0f65f989a8282136cc0beb0a
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-07-13 18:41:09 -0400

    Complete problem 1_data_types

--- ruby_basics/1_data_types/exercises/numbers_exercises.rb
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -4,33 +4,41 @@ def add(a, b)
 end
 
 def subtract(a, b)
+  a-b
   # return the result of subtracting b from a
 end
 
 def multiply(a, b)
+  a*b
   # return the result of multiplying a times b
 end
 
 def divide(a, b)
+  a/b
   # return the result of dividing a by b
 end
 
 def remainder(a, b)
+  a%b
   # return the remainder of dividing a by b using the modulo operator
 end
 
 def float_division(a, b)
+  a.to_f / b.to_f
   # return the result of dividing a by b as a float, rather than an integer
 end
 
 def string_to_number(string)
+  string.to_i
   # return the result of converting a string into an integer
 end
 
 def even?(number)
+  number.even?
   # return true if the number is even (hint: use integer's even? method)
 end
 
 def odd?(number)
+  number.odd?
   # return true if the number is odd (hint: use integer's odd? method)
 end
--- ruby_basics/1_data_types/exercises/string_exercises.rb
+++ ruby_basics/1_data_types/exercises/string_exercises.rb
@@ -4,37 +4,46 @@ def concatenate_example(string)
 end
 
 def concatenate(string)
+  "Hello " + string + "!"
   # use concatenation to format the result to be "Hello <string>!"
 end
 
 def substrings(word)
+  word[0,4]
   # return the first 4 letters from the word using substrings
 end
 
 def capitalize(word)
+  word[0].upcase + word[1...]
   # capitalize the first letter of the word
 end
 
 def uppercase(string)
+  string.upcase
   # uppercase all letters in the string
 end
 
 def downcase(string)
+  string.downcase
   # downcase all letters in the string
 end
 
 def empty_string(string)
+  string.empty?
   # return true if the string is empty
 end
 
 def string_length(string)
+  string.length
   # return the length of the string
 end
 
 def reverse(string)
+  string.reverse
   # return the same string, with all of its characters reversed
 end
 
 def space_remover(string)
+  string.gsub(" ", "")
   # remove all the spaces in the string using gsub
 end
